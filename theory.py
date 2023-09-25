b6fa57d76be1dcd4732556e784a824432fcad89e
alexpech <9056100101@mail.ru>
2023-09-25 17:25:09 +0300

    вспомогательные файлы для вопросов

---
+++ theory.py
@@ -0,0 +1,17 @@
+# text1 = '23'
+# text2 = '33'
+# print(text1 + text2)
+# name = 'sdbhkhlkjhgflkjhgjhgkjhyty55555'
+# print(len(name))
+# print(name[2])
+# for i in range(len(name)):
+# print(i,name[i])
+# interval = range(100,1000)
+# interval = list(interval)
+# print(interval)
+interval = range(500, 550)
+interval = list(interval) 
+print(interval)
+print('G')
+print('M')
+print('UM')
