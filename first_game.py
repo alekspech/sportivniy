a748e19824ce490b275da54f5f68f46956d3d6d8
alexpech <9056100101@mail.ru>
2024-05-10 11:25:21 +0300

    добавлены коментарии

--- first_game.py
+++ first_game.py
@@ -1,40 +1,45 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
+screen = pygame.display.set_mode((1280,720)) #TODO вынести ширину и высоту экрана в переменные 
 clock = pygame.time.Clock()
-running = True
+is_game_running = True
 dt = 0
 
 player_position = pygame.Vector2(
-    screen.get_width()/2,
+    # screen.get_width()/2,
+    0,
     screen.get_height()/2,
-)
+) # spawn position
 
-while running:
+while is_game_running: # основной цикл игры
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            is_game_running = False
 
     screen.fill('blue')
-    pygame.draw.circle(
+    pygame.draw.circle( 
         screen,
         'green',
         player_position,
         radius = 20
-    )
+    ) # cоздание игрока
+    player_speed = 200
+    
+
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_position.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_position.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_position.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_position.x += 300 * dt
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
+        player_position.y -= player_speed * dt 
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        player_position.y += player_speed * dt
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        player_position.x -= player_speed * dt
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        player_position.x += player_speed * dt
+
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000
-    print(dt)
+
 
 pygame.quit()
