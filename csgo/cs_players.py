0e8b334153cce9af829044efcde4dbe7f13c736d
alekspech <9056100101@mail.ru>
2026-02-23 18:35:20 +0300

    функции для имен команд

--- csgo/cs_players.py
+++ csgo/cs_players.py
@@ -17,8 +17,44 @@ with open(data_path, 'r') as f:
 # print(player_match['team'])
 # player_pretty_name = crasivoe_name(player_match)
 # print(player_pretty_name)
-for i in range(len(cs_data)):
-    player_match = cs_data[i]
-    player_pretty_name = crasivoe_name(player_match)
-    if player_match['player_name'] =='s1mple':
-        print(player_pretty_name)
+# for i in range(len(cs_data)):
+#     player_match = cs_data[i]
+#     player_pretty_name = crasivoe_name(player_match)
+#     if player_match['player_name'] =='s1mple':
+#         print(player_pretty_name)
+
+
+
+# for i in range(len(cs_data)):
+    # match_id = cs_data[i]
+    # player_match = cs_data[i]
+    # player_pretty_name = crasivoe_name(player_match)
+    # if match_id ['match_id']== 2339390:
+        # print(player_pretty_name)
+
+def team_info(cs_data, team_name):
+    player_counter = 0        
+    player_names = []
+    for i in range(len(cs_data)):
+        player_match = cs_data[i]
+        player_pretty_name = crasivoe_name(player_match)
+        if player_match['team']== team_name:
+            print(player_pretty_name)
+            player_counter += 1
+            player_names.append(player_match['player_name'])
+    print('players', player_counter)
+    player_names = set(player_names)        
+    print(player_names)
+    return player_names
+
+def get_team_names(cs_data):
+    names = []
+    for i in range(len(cs_data)):
+        names.append(cs_data[i]['team'])
+        names.append(cs_data[i]['opponent'])
+    names = set(names)
+    return names
+
+team_info(cs_data, 'FaZe')
+teams = get_team_names(cs_data)
+pprint(teams)
