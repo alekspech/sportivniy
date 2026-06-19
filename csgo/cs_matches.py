baa32ba7f983d7421fdf89850732c03fcd2f4618
alekspech <9056100101@mail.ru>
2026-06-19 15:12:30 +0300

    дз

--- csgo/cs_matches.py
+++ csgo/cs_matches.py
@@ -49,9 +49,40 @@ def calculate_team_kills(cs_match):
             out[team_name] = 0
         out[team_name] += player_match['kills']
     return out
+def calculate_team_assists(cs_match):
+    out = {}
+    for player_match in cs_match['players']:
+        team_name = player_match['team']
+        if team_name not in out:
+            out[team_name] = 0
+        out[team_name] += player_match['assists']
+    return out
+def calculate_team_deaths(cs_match):
+    out = {}
+    for player_match in cs_match['players']:
+        team_name = player_match['team']
+        if team_name not in out:
+            out[team_name] = 0
+        out[team_name] += player_match['deaths']
+    return out
 
 # pprint(calculate_team_kills(match))
 
 for match_id in cs_matches.keys():
     match = cs_matches[match_id]
-    pprint(calculate_team_kills(match))
+    # pprint(calculate_team_kills(match))
+    # pprint(calculate_team_assists(match))
+    # pprint(calculate_team_deaths(match))
+
+a = input("Выберите что вывести:"
+      "1 это kills"
+      "2 это deaths"
+      "3 это assists"
+      )
+while True:
+    if a == 1:
+        pprint(calculate_team_kills(match))
+    elif a == 2:
+        pprint(calculate_team_deaths(match))
+    elif a == 3:
+        pprint(calculate_team_assists(match))
